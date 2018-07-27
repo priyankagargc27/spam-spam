bb253b2e0c3d1d1a0b35e2f2f906814267a98984
priyankagargc27 <40770470+priyankagargc27@users.noreply.github.com>
2018-07-27 11:37:02 -0500

    spam

---
+++ main.js
@@ -0,0 +1,18 @@
+// let newCustomer = customers.filter((item)=>{
+//    for (let i=0;i<item.length; i++){
+
+//     let strString=item[i].includes("email");
+//     return strString;}
+//  })
+//  console.log(newCustomer)
+
+// console.log("Hello")
+
+// const email=[];
+
+// for (let i=0; i< customers.contacts.emails; i++)
+// {
+
+
+// }
+
